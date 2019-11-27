e6cbeffb382b3d799956371d54022ac0f00a229e
raynear <raynear@gmail.com>
2019-11-27 19:01:45 +0900

    role 적용해서 diamond 페이지 보여주는데 까지 검증

--- src/Diamond.tsx
+++ src/Diamond.tsx
@@ -5,6 +5,7 @@ import { Typography, Container, Grid, Paper, Button } from "@material-ui/core";
 import Web3 from "web3";
 
 import useStyles from "./Style";
+import { playerRoleContractAddress, playerRoleABI } from "./SmartContract";
 // import { ContractAddress, ContractABI } from "./ContractInfo";
 
 
@@ -15,8 +16,27 @@ function Diamond({ match }: any) {
   //  const id = match.params.id;
 
   const [values, setValues] = useState({ clarity: '', cut: '', carat: '', color: '', price: 0, id: '', celler: '', certinfo: '' })
+  const [role, setRole] = useState(0);
+  const [inMyList, setInMyList] = useState(false);
 
   useEffect(() => {
+    const myAddress = (window as any).web3.eth.accounts[0];
+    const mycontract = (window as any).web3.eth.contract(playerRoleABI);
+    const contract = mycontract.at(playerRoleContractAddress);
+
+    (window as any).web3.eth.defaultAccount = (window as any).web3.eth.accounts[0]
+    contract.checkPlayerRole(myAddress, (e: any, r: any) => {
+      if (r[0].toNumber() === 1) {
+        setRole(1);
+      } else if (r[1].toNumber() === 1) {
+        setRole(2);
+      } else if (r[2].toNumber() === 1) {
+        setRole(3);
+      }
+    })
+
+    setInMyList(inMyCookieList(parseInt(match.params.ID, 10)));
+
     setValues({ clarity: 'VVS', cut: 'Good', carat: '3/4', color: 'F', price: 4000000, id: match.params.ID, celler: 'AA 도매상', certinfo: 'BB에서 인증 되었음' });
   }, [])
 
@@ -25,6 +45,22 @@ function Diamond({ match }: any) {
       (window as any).web3 = new Web3((window as any).web3.currentProvider);
     }
   });
+
+  function inMyCookieList(id: number) {
+    const Cookies = document.cookie.split(";");
+    for (const i in Cookies) {
+      if (Cookies[i].split("=")[0].trim() === "MyDiaList") {
+        const DiaList = JSON.parse(Cookies[i].split("=")[1]);
+        console.log(DiaList, id);
+        if (DiaList.includes(id)) {
+          console.log("true");
+          return true;
+        }
+      }
+    }
+    console.log("false");
+    return false;
+  }
 
   function submit() {
     console.log((window as any).web3.currentProvider.selectedAddress);
@@ -37,10 +73,12 @@ function Diamond({ match }: any) {
         return r;
       }
     });
+    inMyCookieList(match.params.ID);
   }
 
   function pushCookie() {
     document.cookie = "MyDiaList=[1,3,5,8,10]"
+    inMyCookieList(match.params.ID);
   }
 
   return (
@@ -83,18 +121,27 @@ function Diamond({ match }: any) {
                     Color: {values.color}
                   </Typography>
                 </Grid>
-                <Grid item={true} className={classes.grid} xs={12} md={12} lg={12}>
-                  <Button fullWidth={true} variant="contained" color="primary" onClick={submit}>살께요!</Button>
-                </Grid>
-                <Grid item={true} className={classes.grid} xs={12} md={12} lg={12}>
-                  <Button fullWidth={true} variant="contained" color="primary" onClick={submit}>판매 리스트에 올리기!</Button>
-                </Grid>
-                <Grid item={true} className={classes.grid} xs={6} md={6} lg={6}>
-                  <Button fullWidth={true} variant="contained" color="primary" onClick={submit}>환불!</Button>
-                </Grid>
-                <Grid item={true} className={classes.grid} xs={6} md={6} lg={6}>
-                  <Button fullWidth={true} variant="contained" color="primary" onClick={submit}>판매완료(송금)!</Button>
-                </Grid>
+                {(role === 1 && inMyList) &&
+                  <Grid item={true} className={classes.grid} xs={12} md={12} lg={12}>
+                    <Button fullWidth={true} variant="contained" color="primary" onClick={submit}>판매 리스트에 올리기!</Button>
+                  </Grid>
+                }
+                {(role === 2 && !inMyList) &&
+                  <Grid item={true} className={classes.grid} xs={12} md={12} lg={12}>
+                    <Button fullWidth={true} variant="contained" color="primary" onClick={submit}>살께요!</Button>
+                  </Grid>
+                }
+                {(role === 2 && inMyList) &&
+                  <>
+                    <Grid item={true} className={classes.grid} xs={6} md={6} lg={6}>
+                      <Button fullWidth={true} variant="contained" color="primary" onClick={submit}>환불!</Button>
+                    </Grid>
+                    <Grid item={true} className={classes.grid} xs={6} md={6} lg={6}>
+                      <Button fullWidth={true} variant="contained" color="primary" onClick={submit}>판매완료(송금)!</Button>
+                    </Grid>
+                  </>
+                }
+
                 <br />
                 <br />
                 <br />
@@ -107,6 +154,7 @@ function Diamond({ match }: any) {
           </Grid>
         </Grid>
       </Container>
+      {role}{"-"}{inMyList.toString()}
     </>
   );
 }
--- src/SmartContract.tsx
+++ src/SmartContract.tsx
@@ -1,3 +1,209 @@
+const playerRoleContractAddress = "0xFCd8f415ce3eC782501a89137A9b476e91dafa83";
+const playerRoleABI = [
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "address[]",
+        "name": "_gemologists",
+        "type": "address[]"
+      }
+    ],
+    "name": "addGemologists",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "address[]",
+        "name": "_retailers",
+        "type": "address[]"
+      }
+    ],
+    "name": "addRetailers",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "address[]",
+        "name": "_wholesalers",
+        "type": "address[]"
+      }
+    ],
+    "name": "addWholesalers",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      }
+    ],
+    "name": "checkGemologist",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      }
+    ],
+    "name": "checkRetailer",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      }
+    ],
+    "name": "checkWholesaler",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      }
+    ],
+    "name": "checkPlayerRole",
+    "outputs": [
+      {
+        "internalType": "enum PlayerRole.Role[]",
+        "name": "",
+        "type": "uint8[]"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "isOwner",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
 const contractAddress = "0xc4be81B466F8cd2Eb2C245E58e28bbA7F571E33e";
 const abi = [
   {
@@ -115,4 +321,4 @@ const abi = [
   }
 ];
 
-export { contractAddress, abi };
+export { playerRoleContractAddress, playerRoleABI, contractAddress, abi };
